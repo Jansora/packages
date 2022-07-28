92e7cb87fab6f8ea1260b67887bae27b711b3dfe
Jansora <zhangyue1936@gmail.com>
2022-07-28 10:58:20 +0800

    modify package project

--- maven/core/src/main/java/com/jansora/app/repo/core/carrier/valobj/AuthValObj.java
+++ maven/core/src/main/java/com/jansora/app/repo/core/carrier/valobj/AuthValObj.java
@@ -13,28 +13,6 @@ import java.io.Serializable;
  * @CreateDate 2022/7/28 AM09:32 <br>
  * @since 1.0 <br>
  */
-public class AuthValObj implements Serializable {
+public record AuthValObj(Long authId, String authToken) implements Serializable {
 
-    Long authId;
-
-    String authToken;
-
-    public AuthValObj() {
-    }
-
-    public Long getAuthId() {
-        return this.authId;
-    }
-
-    public void setAuthId(Long authId) {
-        this.authId = authId;
-    }
-
-    public String getAuthToken() {
-        return this.authToken;
-    }
-
-    public void setAuthToken(String authToken) {
-        this.authToken = authToken;
-    }
 }
