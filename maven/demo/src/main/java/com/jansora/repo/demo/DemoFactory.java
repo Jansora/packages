67dc17733b3acb842ec236728d5e9a196cdccaa1
Jansora <zhangyue1936@gmail.com>
2022-07-28 16:49:58 +0800

    add release version

---
+++ maven/demo/src/main/java/com/jansora/repo/demo/DemoFactory.java
@@ -0,0 +1,17 @@
+package com.jansora.repo.demo;
+
+/**
+ * <Description> Description for DemoFactory <br>
+ *
+ * @author jansora (zhang.yangyuan) <br>
+ * @version 1.0 <br>
+ * @email zhangyue1936@gmail.com
+ * @transId null
+ * @CreateDate 2022/7/20 AM09:37 <br>
+ * @since 1.0 <br>
+ */
+public interface DemoFactory {
+
+    void run(String[] args) throws Throwable;
+
+}
--- maven/spring/src/main/java/com/jansora/repo/spring/SpringContext.java
+++ maven/spring/src/main/java/com/jansora/repo/spring/SpringContext.java
@@ -1,4 +1,4 @@
-package com.jansora.app.repo.spring;
+package com.jansora.repo.spring;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
