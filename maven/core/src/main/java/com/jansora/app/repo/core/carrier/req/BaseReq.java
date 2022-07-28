6b52d52bb27d0309aa40b8e52a75f54f6d3de01f
Jansora <zhangyue1936@gmail.com>
2022-07-28 09:45:38 +0800

    init .

---
+++ maven/core/src/main/java/com/jansora/app/repo/core/carrier/req/BaseReq.java
@@ -0,0 +1,16 @@
+package com.jansora.app.repo.core.carrier.req;
+
+import java.io.Serializable;
+
+/**
+ * <Description> Description for BaseReq <br>
+ *
+ * @author jansora (zhang.yangyuan) <br>
+ * @version 1.0 <br>
+ * @email zhangyue1936@gmail.com
+ * @transId null
+ * @CreateDate 2022/7/28 AM09:35 <br>
+ * @since 1.0 <br>
+ */
+public class BaseReq implements Serializable {
+}
