ba839d113a2feaeac42e7be46426ed0cf86548a2
Jansora <zhangyue1936@gmail.com>
2022-07-29 14:36:24 +0800

    [repo]: update repo version (toString add indent)

--- maven/core/src/main/java/com/jansora/app/repo/core/utils/JsonUtils.java
+++ maven/core/src/main/java/com/jansora/app/repo/core/utils/JsonUtils.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.jansora.app.repo.core.exception.transform.FormatException;
 import org.slf4j.Logger;
@@ -29,7 +30,7 @@ public class JsonUtils {
     private static final ObjectMapper instance = new ObjectMapper();
 
     static {
-//        instance.enable(SerializationFeature.INDENT_OUTPUT);
+        instance.enable(SerializationFeature.INDENT_OUTPUT);
         instance.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
