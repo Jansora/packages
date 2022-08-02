e35b655a512089708a0be24867cb502ea0925ebd
Jansora <zhangyue1936@gmail.com>
2022-08-02 11:01:42 +0800

    [core]: modify carrier

--- maven/core/src/main/java/com/jansora/app/repo/core/carrier/req/BaseEtyReq.java
+++ maven/core/src/main/java/com/jansora/app/repo/core/carrier/req/BaseEtyReq.java
@@ -1,16 +1,16 @@
 package com.jansora.app.repo.core.carrier.req;
 
-import com.jansora.app.repo.core.carrier.dto.BaseDto;
+import com.jansora.app.repo.core.carrier.ety.BaseEty;
 
 /**
- * <Description> Description for BaseReq <br>
+ * <Description> Description for BaseEtyReq <br>
  *
  * @author jansora (zhang.yangyuan) <br>
  * @version 1.0 <br>
  * @email zhangyue1936@gmail.com
  * @transId null
- * @CreateDate 2022/8/1 AM11:58 <br>
+ * @CreateDate 2022/8/2 AM11:01 <br>
  * @since 1.0 <br>
  */
-public class BaseReq extends BaseDto {
+public class BaseEtyReq extends BaseEty {
 }
---
+++ maven/core/src/main/java/com/jansora/app/repo/core/carrier/vo/BaseEtyVo.java
@@ -0,0 +1,17 @@
+package com.jansora.app.repo.core.carrier.vo;
+
+import com.jansora.app.repo.core.carrier.ety.BaseEty;
+
+/**
+ * <Description> Description for BaseEtyVo <br>
+ *
+ * @author jansora (zhang.yangyuan) <br>
+ * @version 1.0 <br>
+ * @email zhangyue1936@gmail.com
+ * @transId null
+ * @CreateDate 2022/8/1 AM11:59 <br>
+ * @since 1.0 <br>
+ */
+public class BaseEtyVo extends BaseEty {
+
+}
