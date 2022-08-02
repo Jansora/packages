1e5685e024d094a9862a05110c7bc8199b8fee45
Jansora <zhangyue1936@gmail.com>
2022-08-02 10:37:53 +0800

    [core]: add carrier

---
+++ maven/core/src/main/java/com/jansora/app/repo/core/carrier/ety/BaseEty.java
@@ -0,0 +1,16 @@
+package com.jansora.app.repo.core.carrier.ety;
+
+import com.jansora.app.repo.core.carrier.dto.BaseDto;
+
+/**
+ * <Description> Description for BaseEty <br>
+ *
+ * @author jansora (zhang.yangyuan) <br>
+ * @version 1.0 <br>
+ * @email zhangyue1936@gmail.com
+ * @transId null
+ * @CreateDate 2022/8/2 AM10:35 <br>
+ * @since 1.0 <br>
+ */
+public class BaseEty extends BaseDto {
+}
---
+++ maven/core/src/main/java/com/jansora/app/repo/core/carrier/valobj/BaseValObj.java
@@ -0,0 +1,16 @@
+package com.jansora.app.repo.core.carrier.valobj;
+
+import com.jansora.app.repo.core.carrier.dto.BaseDto;
+
+/**
+ * <Description> Description for BaseValObj <br>
+ *
+ * @author jansora (zhang.yangyuan) <br>
+ * @version 1.0 <br>
+ * @email zhangyue1936@gmail.com
+ * @transId null
+ * @CreateDate 2022/8/2 AM10:36 <br>
+ * @since 1.0 <br>
+ */
+public class BaseValObj extends BaseDto {
+}
