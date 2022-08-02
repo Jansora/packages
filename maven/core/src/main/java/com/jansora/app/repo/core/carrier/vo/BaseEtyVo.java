dbcb51f1d05d62680b6e81fc7f0ca131fef09bf6
Jansora <zhangyue1936@gmail.com>
2022-08-02 15:18:06 +0800

    [dubbo]: add dubbo module

--- maven/core/src/main/java/com/jansora/app/repo/core/carrier/vo/BaseEtyVo.java
+++ maven/core/src/main/java/com/jansora/app/repo/core/carrier/vo/BaseEtyVo.java
@@ -15,4 +15,32 @@ public class BaseEtyVo extends BaseVo {
      * 唯一标识
      */
     private Long id;
+
+    private String createdAt;
+
+    private String updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
