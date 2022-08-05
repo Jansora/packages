d62356788d3bf463b35d72b91da95c6fe7fbcb4d
Jansora <zhangyue1936@gmail.com>
2022-08-05 12:04:06 +0800

    [core]: modify carrier add search

---
+++ maven/core/src/main/java/com/jansora/app/repo/core/carrier/vo/SearchVo.java
@@ -0,0 +1,105 @@
+package com.jansora.app.repo.core.carrier.vo;
+
+/**
+ * <Description> Description for SearchVo <br>
+ *
+ * @author jansora (zhang.yangyuan) <br>
+ * @version 1.0 <br>
+ * @email zhangyue1936@gmail.com
+ * @transId null
+ * @CreateDate 2022/8/5 AM11:26 <br>
+ * @since 1.0 <br>
+ */
+public class SearchVo extends BaseEtyVo {
+
+    /**
+     * 名称
+     */
+    protected String name;
+
+    /**
+     * 是否启用
+     */
+    protected Boolean enabled;
+
+    /**
+     * 分类
+     */
+    protected String classify;
+
+    /**
+     * 标签
+     */
+    protected String tag;
+
+    /**
+     * 标签
+     */
+    protected String description;
+
+    /**
+     * logo
+     */
+    protected String logo;
+
+    /**
+     * 标签
+     */
+    protected Long userId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getClassify() {
+        return classify;
+    }
+
+    public void setClassify(String classify) {
+        this.classify = classify;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
