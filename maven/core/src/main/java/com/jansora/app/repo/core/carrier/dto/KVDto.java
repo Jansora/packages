329be8b2a51cf08cfb3a58009ed0cd519dfeb50c
Jansora <zhangyue1936@gmail.com>
2022-08-05 12:19:06 +0800

    [core]: modify carrier kv dto

--- maven/core/src/main/java/com/jansora/app/repo/core/carrier/dto/KVDto.java
+++ maven/core/src/main/java/com/jansora/app/repo/core/carrier/dto/KVDto.java
@@ -12,16 +12,16 @@ package com.jansora.app.repo.core.carrier.dto;
  */
 public class KVDto<T> extends BaseDto {
 
-    String name;
+    String key;
 
     T value;
 
-    public String getName() {
-        return name;
+    public String getKey() {
+        return key;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setKey(String key) {
+        this.key = key;
     }
 
     public T getValue() {
