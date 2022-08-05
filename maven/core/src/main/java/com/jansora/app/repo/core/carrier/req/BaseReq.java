10c40beb3b41f04dda8de1a5d2d7384f548b80bc
Jansora <zhangyue1936@gmail.com>
2022-08-05 16:02:48 +0800

    [core]: modify carrier

--- maven/core/src/main/java/com/jansora/app/repo/core/carrier/req/BaseReq.java
+++ maven/core/src/main/java/com/jansora/app/repo/core/carrier/req/BaseReq.java
@@ -13,4 +13,30 @@ import com.jansora.app.repo.core.carrier.dto.BaseDto;
  * @since 1.0 <br>
  */
 public class BaseReq extends BaseDto {
+
+    /**
+     * 认证id
+     */
+    Long authId;
+
+    /**
+     * 认证 token
+     */
+    String authToken;
+
+    public Long getAuthId() {
+        return authId;
+    }
+
+    public void setAuthId(Long authId) {
+        this.authId = authId;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
 }
