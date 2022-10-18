7bf5ae0dc9788562b79a30ae32518a3346fd1752
Jansora <zhangyue1936@gmail.com>
2022-10-18 10:07:24 +0800

    add boot

--- maven/monitor/src/main/java/com/jansora/repo/monitor/autoconfiguration/MonitorAutoConfiguration.java
+++ maven/monitor/src/main/java/com/jansora/repo/monitor/autoconfiguration/MonitorAutoConfiguration.java
@@ -1,4 +1,4 @@
-package com.jansora.repo.monitor.autoconfiguration.autoconfiguration;
+package com.jansora.repo.monitor.autoconfiguration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
