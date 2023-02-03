b34912227cfb8d5e4ab0fb15e5eab7cdcfedd203
Jansora <zhangyue1936@gmail.com>
2023-02-03 16:50:58 +0800

    modify global 实现方式

--- npm/@jansora/global/src/mount/MountColor.jsx
+++ npm/@jansora/global/src/mount/MountColor.jsx
@@ -5,7 +5,7 @@
 * @date 2019-12-17 10:39
 */
 import {useContext, useEffect} from 'react';
-import {COLOR, COLOR_LIST} from "../constant/global";
+import {COLOR} from "../constant/global";
 import {GlobalStore} from "../store/global";
 
 
@@ -14,9 +14,10 @@ const MountColor = () => {
   const { color } = useContext(GlobalStore);
 
   useEffect(() => {
-    COLOR_LIST.forEach(
-        cur => document.body.classList.remove([cur.color]))
-    document.body.classList.add(color);
+    // COLOR_LIST.forEach(
+    //     cur => document.body.classList.remove([cur.color]))
+    // document.body.classList.add(color);
+    document.documentElement.setAttribute(COLOR, color);
     localStorage.setItem(COLOR, color)
   }, [color]);
 
--- npm/@jansora/global/src/mount/MountTheme.jsx
+++ npm/@jansora/global/src/mount/MountTheme.jsx
@@ -5,7 +5,7 @@
 * @date 2019-12-17 10:39
 */
 import {useContext, useEffect} from 'react';
-import {THEME, THEME_LIST} from "../constant/global";
+import {THEME} from "../constant/global";
 import {GlobalStore} from "../store/global";
 
 
@@ -14,9 +14,11 @@ const MountTheme = () => {
   const { theme } = useContext(GlobalStore);
 
   useEffect(() => {
-    THEME_LIST.forEach(
-        cur => document.body.classList.remove([cur.theme]))
-    document.body.classList.add(theme);
+    // THEME_LIST.forEach(
+    //     cur => document.body.classList.remove([cur.theme]))
+    // document.body.classList.add(theme);
+
+    document.documentElement.setAttribute(THEME, theme);
     localStorage.setItem(THEME, theme)
   }, [theme]);
 
