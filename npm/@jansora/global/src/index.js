4b47a73ad2e1e262a3617d25f7098e51542dc34c
Jansora <zhangyue1936@gmail.com>
2023-02-07 14:11:32 +0800

    theme init completed

--- npm/@jansora/global/src/index.js
+++ npm/@jansora/global/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import GlobalStoreProvider from "./store/global";
-import './theme.css';
-import './init.css'
-import './override.css'
+
+import './init.less'
 
 export default GlobalStoreProvider;
