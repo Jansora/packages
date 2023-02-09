e9f3df405b6adae828954a4f47da1f1b80ec7917
Jansora <zhangyue1936@gmail.com>
2023-02-09 12:30:03 +0800

    update package

--- npm/@jansora/ui/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-import './init.less'
-
-export default GlobalStoreProvider;
--- npm/@jansora/ui/src/styled/base/FlexPadding.jsx
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-const FlexPadding = styled.div`
-  flex: 1 1 auto;
-`
-export default FlexPadding;
