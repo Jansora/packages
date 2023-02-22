3421c38e101188429422d08876caad671d47cb54
Jansora <zhangyue1936@gmail.com>
2023-02-22 10:17:05 +0800

    fix monaco loading failed.

---
+++ npm/@jansora/monaco/src/editor/LazyLoadEditor.js
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react';
+
+const LazyLoadEditor = () => {
+    const documentLoaded = document.querySelector("#init-monaco-editor") != null;
+
+    // const monacoRef = useRef(window.monaco);
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(() => {
+        setLoading(true)
+        const interval = setInterval(() => {
+            // console.log("loading", window.monaco)
+            if (window.monaco) {
+                setLoading(false)
+                clearInterval(interval)
+            }
+        }, 100)
+    }, [])
+
+    // console.log("documentLoaded", documentLoaded, !loading)
+    if (!documentLoaded) {
+        const wrapper = document.createElement('div');
+        wrapper.setAttribute("id", "init-monaco-editor")
+        document.body.appendChild(wrapper);
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.src = 'https://cdn.jansora.com/lib/monaco-editor/0.21.2/min/vs/monaco-editor-loader-proxy.js';
+        wrapper.appendChild(script);
+    }
+    return !loading;
+}
+
+export default LazyLoadEditor;
