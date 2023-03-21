034665e308c6bfbf57527b9d4ff63bc1496e2178
Jansora <zhangyue1936@gmail.com>
2023-03-21 22:15:16 +0800

    add elasticsearch package

---
+++ maven/elasticsearch/src/main/java/com/jansora/repo/elasticsearch/autoconfiguration/ElasticsearchAutoConfiguration.java
@@ -0,0 +1,21 @@
+package com.jansora.repo.elasticsearch.autoconfiguration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+/**
+ * <Description> Description for SpringAutoConfiguration <br>
+ *
+ * @author jansora (zhang.yangyuan) <br>
+ * @version 1.0 <br>
+ * @CreateDate 2021/11/19 11:50:48 <br>
+ * @since 1.0 <br>
+ */
+@Configuration
+@ComponentScan(basePackages = "com.jansora.repo.elasticsearch")
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class ElasticsearchAutoConfiguration {
+
+}
---
+++ maven/elasticsearch/src/main/java/com/jansora/repo/elasticsearch/factory/ElasticsearchCrudFactory.java
@@ -0,0 +1,14 @@
+package com.jansora.repo.elasticsearch.factory;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+/**
+ * @description:
+ * @author: jansora (zhang.yangyuan)
+ * @date: 2023-03-21 22:10:49
+ */
+@NoRepositoryBean
+public interface ElasticsearchCrudFactory<T, ID> extends ElasticsearchRepository<T, ID> {
+
+}
