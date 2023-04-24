49c0cd35500bc8216220c57897926264f02349c7
Jansora <zhangyue1936@gmail.com>
2023-04-24 17:12:59 +0800

    release 1.100.37 optimize entity factory

--- maven/core/src/main/java/com/jansora/repo/core/payload/response/SearchResponse.java
+++ maven/core/src/main/java/com/jansora/repo/core/payload/response/SearchResponse.java
@@ -1,9 +1,7 @@
 package com.jansora.repo.core.payload.response;//package com.jansora.repo.core.payload.vo;
 
-import com.jansora.repo.core.factory.entity.EntityResponseFactory;
 import lombok.Data;
-
-import java.util.Date;
+import lombok.EqualsAndHashCode;
 
 /**
  * <Description> Description for SearchVo <br>
@@ -15,23 +13,9 @@ import java.util.Date;
  * @CreateDate 2022/8/5 AM11:26 <br>
  * @since 1.0 <br>
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class SearchResponse implements EntityResponseFactory {
-
-    /**
-     * 唯一标识
-     */
-    private Long id;
-
-    /**
-     * 创建时间
-     */
-    private Date createdAt;
-
-    /**
-     * 更新时间
-     */
-    private Date updatedAt;
+public class SearchResponse extends EntityResponse {
 
     /**
      * 名称
@@ -67,22 +51,5 @@ public class SearchResponse implements EntityResponseFactory {
      * 标签
      */
     protected Long userId;
-
-//    public static <T extends SearchResponse, S extends SearchableDo> void overrideSearch(T target, S source) {
-//
-//        overrideBase(target, source);
-//
-//        // 搜索信息
-//        target.setName(source.getName());
-//        target.setUserId(source.getUserId());
-//        target.setClassify(source.getClassify());
-//        target.setTag(source.getTag());
-//
-//        // 搜索表信息
-//        target.setEnabled(source.getEnabled());
-//        target.setDescription(source.getDescription());
-//        target.setLogo(source.getLogo());
-//
-//    }
 
 }
