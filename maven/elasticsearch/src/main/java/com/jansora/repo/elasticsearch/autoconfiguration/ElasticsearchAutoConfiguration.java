a2cf8eefc52c8219bdf53fad290b1bad6e462988
zhangyy53 <zhangyue1936@gmail.com>
2023-12-21 14:16:32 +0800

    ci package 3.100.2 / modify elasticsearch deactivation time

--- maven/elasticsearch/src/main/java/com/jansora/repo/elasticsearch/autoconfiguration/ElasticsearchAutoConfiguration.java
+++ maven/elasticsearch/src/main/java/com/jansora/repo/elasticsearch/autoconfiguration/ElasticsearchAutoConfiguration.java
@@ -1,9 +1,16 @@
 package com.jansora.repo.elasticsearch.autoconfiguration;
 
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.client.RestClientBuilder;
+import org.springframework.boot.autoconfigure.elasticsearch.RestClientBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+
+import java.time.Duration;
 
 /**
  * <Description> Description for SpringAutoConfiguration <br>
@@ -17,5 +24,26 @@ import org.springframework.core.annotation.Order;
 @ComponentScan(basePackages = "com.jansora.repo.elasticsearch")
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class ElasticsearchAutoConfiguration {
+
+    @Bean
+    RestClientBuilderCustomizer keepAliveCustomizer() {
+        return new KeepAliveCustomizer();
+    }
+
+    static class KeepAliveCustomizer implements RestClientBuilderCustomizer {
+        @Override
+        public void customize(RestClientBuilder builder) {
+
+        }
+
+        @Override
+        public void customize(HttpAsyncClientBuilder builder) {
+            builder.setKeepAliveStrategy((response, context) -> Duration.ofMinutes(5).toMillis());
+        }
+
+        @Override
+        public void customize(RequestConfig.Builder builder) {
+        }
+    }
 
 }
