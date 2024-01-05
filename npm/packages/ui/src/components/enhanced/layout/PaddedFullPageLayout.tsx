ce67a644c7cf8a0d6545993d5a4fe7ed531ea3a4
zhangyy53 <zhangyue1936@gmail.com>
2024-01-05 11:12:29 +0800

    ci version@0.0.64  modify layout

--- npm/packages/ui/src/components/enhanced/layout/PaddedFullPageLayout.tsx
+++ npm/packages/ui/src/components/enhanced/layout/PaddedFullPageLayout.tsx
@@ -2,14 +2,15 @@ import React from "react";
 
 import {FunctionComponentProps} from "../../../lib/declares";
 import PageLayout from "./PageLayout";
+import {cn} from "../../../lib/utils";
 
 
-const PaddedFullPageLayout = ({children}: FunctionComponentProps) => {
+const PaddedFullPageLayout = ({children, className}: FunctionComponentProps) => {
 
 
     return (
         <PageLayout>
-            <div className="mt-12 w-full" style={{height: "calc(100vh - 3rem)"}}>
+            <div className={cn("mt-12 w-full", className)} style={{height: "calc(100vh - 3rem)"}}>
                 {children}
             </div>
         </PageLayout>
