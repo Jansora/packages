ff41233cafbce69b64b0e62640d854e1d26c7aa0
zhangyy53 <zhangyue1936@gmail.com>
2024-01-15 10:57:14 +0800

    ci package 3.100.16 / adapter jackson

--- maven/core/src/main/java/com/jansora/repo/core/factory/repository/entity/EntityFactory.java
+++ maven/core/src/main/java/com/jansora/repo/core/factory/repository/entity/EntityFactory.java
@@ -2,15 +2,15 @@ package com.jansora.repo.core.factory.repository.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.jansora.repo.core.payload.entity.BaseEntity;
 
 /**
  * @description:
  * @author: jansora (zhang.yangyuan)
  * @date: 2023-04-24 16:27:23
  */
-@JsonDeserialize(as = BaseEntity.class)
+@JsonDeserialize
 public interface EntityFactory {
+
 
     Long getId();
 
